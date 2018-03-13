7c47e09ae82d2b0dbb6d9f0b7e82c03dd8dd1b74
akornes <andrew.kornes@gmail.com>
2018-03-13 00:32:45 -0400

    submitting articles, next we'll list them.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,26 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
     end
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :text))
 
+    #******edit goes here *******
+
+    def create
+        @article = Article.new(article_params)
+   
         @article.save
         redirect_to @article
     end
+
+    #******update goes here *******
+
+    #******destroy goes here *******
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
